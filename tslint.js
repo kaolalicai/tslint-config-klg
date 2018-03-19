9b95e84babf6bf03b8c4ba8161bba3d4427529af
nick <myfjdthink@163.com>
2018-03-19 11:38:34 +0800

    添加 env 设置，支持 jest mocha es6 node

--- tslint.js
+++ tslint.js
@@ -1,4 +1,10 @@
 module.exports = {
+  env: {
+    "jest": true,
+    "mocha": true,
+    "es6": true,
+    "node": true
+  },
   extends: ['tslint-config-standard'],
   rules: {
     "no-console": [true, "log", "error"],
